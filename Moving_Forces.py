4c2627df300cfb6eb2d8224f8d0ae8c6704606ca
Raven van Ewijk <raven.ewijk@klm.com>
2020-02-19 12:54:54 +0100

    Function now also returns list of moment generated by moved forces

--- Moving_Forces.py
+++ Moving_Forces.py
@@ -5,8 +5,24 @@ Created on Wed Feb 19 12:01:11 2020
 @author: Raven
 """
 
+"""This script moves the forces to the hinge line, and calculates the corresponding moment"""
+
 import numpy as np
 
 from Resultant_Centroid_DistributedLoadCorrect import integrate
-data = np.loadtxt('AERO.dat',delimiter = ',')
+
+#Read data of distributed load magnitude
+data = np.loadtxt('AERO.dat',delimiter = ',')  #Creating an Array from the aerodynamic load data file
+
 Vtot, Centroid_Zlst, Maglst  = integrate(16,16,data)
+
+def integrate_and_move_forces(x_mesh, z_mesh, data):
+    Vtot, Centroid_Zlst, Maglst  = integrate(x_mesh, z_mesh, data)
+    
+    Momentmaglst = []
+    
+    for num1, num2 in zip(Centroid_Zlst, Maglst):
+        Momentmaglst.append(num1 * num2)
+    
+    return Vtot, Centroid_Zlst, Maglst, Momentmaglst
+    
