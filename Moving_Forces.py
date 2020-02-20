1799ce98df9caf6081e5db53936a980aa8c939e1
Raven van Ewijk <raven.ewijk@klm.com>
2020-02-20 14:01:20 +0100

    Added comments

--- Moving_Forces.py
+++ Moving_Forces.py
@@ -14,14 +14,16 @@ from Resultant_Centroid_DistributedLoadCorrect import integrate
 #Read data of distributed load magnitude
 data = np.loadtxt('AERO.dat',delimiter = ',')  #Creating an Array from the aerodynamic load data file
 
-Vtot, Centroid_Zlst, Maglst  = integrate(16,16,data)
 
 def integrate_and_move_forces(x_mesh, z_mesh, data):
+    #Retrieving total resultant force, list if centroids and list of magnitudes
     Vtot, Centroid_Zlst, Maglst  = integrate(x_mesh, z_mesh, data)
     
+    #creating list to store moments
     Momentmaglst = []
     
     for num1, num2 in zip(Centroid_Zlst, Maglst):
+        #calculating moments, equal to each force in Maglst multiplied by their arms --> centroids
         Momentmaglst.append(num1 * num2)
     
     return Vtot, Centroid_Zlst, Maglst, Momentmaglst
