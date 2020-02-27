3b6274f9bede2b9153d13b9ee5cd43066dd21eef
BvDillen <61149588+BvDillen@users.noreply.github.com>
2020-02-27 14:43:27 +0100

    Add files via upload

--- Main_script.py
+++ Main_script.py
@@ -1,3 +1,4 @@
+# Imports
 import numpy as np
 from Interpolation import patchinterpolate
 from NEW_Forces_Deflections import output, locationvalue
@@ -8,40 +9,48 @@ import matplotlib.pyplot as plt
 from Deflections import deflections
 
 
+# Input parameters
 Ca    = 0.505       #Chord Length Aileron 
 la    = 1.611       #Span of the Aileron
 x1    = 0.125       #x-location of hinge 1
 x2    = 0.498       #x-location of hinge 2 
 x3    = 1.494       #x-location of hinge 3
-xa    = 24.5E-2        #Distance between actuator 1 and 2
-h     = 16.1E-2        #Aileron Height 
-tsk   = 1.1E-3         #Skin thickness
-tsp   = 2.4E-3         #Spar Thickness
-tst   = 1.2E-3         #Thickness of stiffener
-hst   = 1.3E-2         #Height of stifffener
-wst   = 1.7E-2         #Width of stiffeners 
+xa    = 24.5E-2     #Distance between actuator 1 and 2
+h     = 16.1E-2     #Aileron Height
+tsk   = 1.1E-3      #Skin thickness
+tsp   = 2.4E-3      #Spar Thickness
+tst   = 1.2E-3      #Thickness of stiffener
+hst   = 1.3E-2      #Height of stifffener
+wst   = 1.7E-2      #Width of stiffeners
 nst   = 11          #Number of stiffeners (equalt spaced)
-d1    = 0.389E-2       #Vertical displacement of hinge 1
-d3    = 1.245E-2       #Vertical displacement of hinge 2
+d1    = 0.389E-2    #Vertical displacement of hinge 1
+d3    = 1.245E-2    #Vertical displacement of hinge 2
 theta = 30          #Maximum upward deflection 
-P     = 49.2E3        #Load in actuator 2 
+P     = 49.2E3      #Load in actuator 2
 E     = 73.1E9      # [Pa]
 G     = 28E9        # [Pa]
-J     = CalcTorsStif(Ca,h,tsk,tsp)
 
+
+# Calculated parameters
+J     = CalcTorsStif(Ca,h,tsk,tsp)
+zsc = -0.0855       # [m]
+Dx, zbar,ybar,Izz,Iyy, stiff = cross_sect_prop(h,tsk,tsp,tst,hst,wst,Ca,nst)
+
+
+# Reading the aerodynamic load
 data = np.loadtxt('AERO.dat',delimiter = ',')
 
+
+# Transforming the coordinates
 xl = []
 zl = []
 Nz = len(data)
 Nx = len(data[0])
-ca = 0.505
-la = 1.611
 
 for i in range(1,Nz+1):  
     thetazi  = (i-1)/Nz*np.pi
     thetazi1 = (i)/Nz*np.pi
-    z  =  -(1/2)*((ca/2)*(1-np.cos(thetazi))+(ca/2)*(1-np.cos(thetazi1)))    
+    z  =  -(1/2)*((Ca/2)*(1-np.cos(thetazi))+(Ca/2)*(1-np.cos(thetazi1)))
     zl.append(z)
 
 for k in range(1,Nx+1):
@@ -51,99 +60,131 @@ for k in range(1,Nx+1):
     xl.append(x)
 
 
-zsc = -0.08
+# Interpolate the aerodynamic load
 xlst, zlst, qlst = patchinterpolate(600,600,xl,zl,data)
-Vlst, Mlst, defllst, Tlst, thetalst = output(xlst, zlst, qlst, zsc)
-Dx, zbar,ybar,Izz,Iyy, stiff = cross_sect_prop(h,tsk,tsp,tst,hst,wst,Ca,nst)
+
+
+# Integrate the aerodynamic load
+Vlst, Mlst, d1lst, defllst, Tlst, thetalst = output(xlst, zlst, qlst, zsc)
+
+# Calculate the reaction forces
 Ay,Az,By,Bz,Cy,Cz,Fy,Fz,C1,C2,C3,C4,C5 = reaction_forces(la,x1,x2,x3,xa,h,d1,d3,theta,P,E,G,zsc,Iyy,
                                                          Izz, J,xlst,Vlst,Mlst,defllst,Tlst,thetalst)
-#Ay = 37833
-#Az = -244102
-#By = -58360
-#Bz = 331972
-#Cy = 17893
-#Cz = -81694
-#F = 42068
-#
-#Fy = F*np.sin(theta*np.pi/180)
-#Fz = F*np.cos(theta*np.pi/180)
-#
-#C1 = -0.011
-#C2 = 0.0048
-#C3 = 0.0068
-#C4 = -0.0028
-#C5 = -0.0032
 
+
+# Creating the plots
 x = xlst #np.arange(0,la+dx,dx)
+
 y = []
-z = []
-twist = []
-Sy = []
-Sz = []
-momenty = []
+slopey = []
 momentz = []
-torque = []
+sheary = []
+
+z = []
+slopez = []
+momenty = []
+shearz = []
+
+twist = []
+torquedist = []
 
 for xi in x:
-    Myx,Mzx,Tx,Syx,Szx,vx,wx,thetax = deflections(xi,x1,x2,x3,xa,h,theta,P,E,G,zsc,Iyy,Izz,J,xlst,Vlst,Mlst,defllst,Tlst,thetalst,Ay,Az,By,Bz,Cy,Cz,Fy,Fz,C1,C2,C3,C4,C5)
-    y.append(vx+thetax*zsc)
-    z.append(wx)
-    twist.append(thetax)
-    momenty.append(Myx)
+    Myx,Mzx,Tx,Syx,Szx,vsx,vx,wsx,wx,thetax = deflections(xi,x1,x2,x3,xa,h,theta,P,E,G,zsc,Iyy,Izz,J,xlst,Vlst,Mlst,d1lst,defllst,Tlst,thetalst,Ay,Az,By,Bz,Cy,Cz,Fy,Fz,C1,C2,C3,C4,C5)
+    y.append(vx)
+    slopey.append(vsx)
     momentz.append(Mzx)
-    torque.append(Tx)
-    Sy.append(Syx)
-    Sz.append(Szx)
-momentz = np.array(momentz)
+    sheary.append(Syx)
 
+    z.append(wx)
+    slopez.append(wsx)
+    momenty.append(Myx)
+    shearz.append(Szx)
 
-plt.subplot(231)
+    twist.append(thetax)
+    torquedist.append(Tx)
+
+# y
+plt.subplot(221)
 plt.plot(x,y)
-plt.title('Vertical Displacement')
+plt.title('Deflection in y direction')
 plt.xlabel('x [m]')
-plt.ylabel('y [m]')
+plt.ylabel('v(x) [m]')
 plt.grid()
 
-
-plt.subplot(232)
-plt.plot(x,z)
-plt.title('Horizontal Displacement')
+plt.subplot(222)
+plt.plot(x,slopey)
+plt.title('Slope in y direction')
 plt.xlabel('x [m]')
-plt.ylabel('z [m]')
+plt.ylabel("v'(x) [/m]")
 plt.grid()
 
-
-plt.subplot(233)
-plt.plot(x,twist)
-plt.title('Twist')
-plt.xlabel('x [m]')
-plt.ylabel('Theta [rad]')
-plt.grid()
-
-
-plt.subplot(234)
-plt.plot(x,momenty)
-plt.title('Moment around the y axis')
-plt.xlabel('x [m]')
-plt.ylabel('My [Nm]')
-plt.grid()
-
-
-plt.subplot(235)
+plt.subplot(223)
 plt.plot(x,momentz)
 plt.title('Moment around the z axis')
 plt.xlabel('x [m]')
-plt.ylabel('Mz [Nm]')
+plt.ylabel('Mz(x) [Nm]')
 plt.grid()
 
+plt.subplot(224)
+plt.plot(x,sheary)
+plt.title('Shear force in y direction')
+plt.xlabel('x [m]')
+plt.ylabel('Sy(x) [N]')
+plt.grid()
 
-plt.subplot(236)
-plt.plot(x,torque)
+plt.show()
+
+# z
+plt.subplot(221)
+plt.plot(x,z)
+plt.title('Deflection in z direction')
+plt.xlabel('x [m]')
+plt.ylabel('w(x) [m]')
+plt.grid()
+
+plt.subplot(222)
+plt.plot(x,slopez)
+plt.title('Slope in z direction')
+plt.xlabel('x [m]')
+plt.ylabel("w'(x) [/m]")
+plt.grid()
+
+plt.subplot(223)
+plt.plot(x,momenty)
+plt.title('Moment around the y axis')
+plt.xlabel('x [m]')
+plt.ylabel('My(x) [Nm]')
+plt.grid()
+
+plt.subplot(224)
+plt.plot(x,shearz)
+plt.title('Shear force in z direction')
+plt.xlabel('x [m]')
+plt.ylabel('Sz(x) [N]')
+plt.grid()
+
+plt.show()
+
+# x
+plt.subplot(221)
+plt.plot(x,z)
+plt.title('Twist')
+plt.xlabel('x [m]')
+plt.ylabel('theta(x) [rad]')
+plt.grid()
+
+plt.subplot(222)
+plt.plot(x,torquedist)
 plt.title('Torque')
 plt.xlabel('x [m]')
-plt.ylabel('T [Nm]')
+plt.ylabel("T(x) [/m]")
 plt.grid()
 
+plt.subplot(223)
+plt.plot(x,thetalst)
+plt.title('Torque distribution')
+plt.xlabel('x [m]')
+plt.ylabel('tau(x) [Nm]')
+plt.grid()
 
-plt.savefig('Results.jpg')
 plt.show()
